f67f029d4296272b919025dd9bf831025c9dad66
Alexander Tverskoy <astverskoy@gmail.com>
2026-07-07 00:48:01 +0300

    Squashed 'libs/toolkit/' content from commit 1818fcf
    git-subtree-dir: libs/toolkit
    git-subtree-split: 1818fcfad5fef3e1016b8ee0f943a70da67512b3

---
+++ toolkit.pas
@@ -0,0 +1,23 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit toolkit;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  GlobalKeyboardHook, GlobalMouseHook, OneShotTimer, TextDropTarget, 
+  LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+  RegisterUnit('TextDropTarget', @TextDropTarget.Register);
+end;
+
+initialization
+  RegisterPackage('toolkit', @Register);
+end.
